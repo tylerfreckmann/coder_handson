92f1004480ce27b71195959a4c56a9148a6a6208
tylerfreckmann <tyler.freckmann@gmail.com>
2022-02-09 12:59:41 -0800

    Update custom_random_forest.py

--- python_library/custom_random_forest.py
+++ python_library/custom_random_forest.py
@@ -1,14 +1,5 @@
-
-# coding: utf-8
-
-import pandas as pd, numpy as np
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-from sklearn.feature_extraction import DictVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import roc_auc_score
 
 param_grid = {
     "max_depth"        : [3, None],
@@ -16,8 +7,8 @@ param_grid = {
     "min_samples_split": [2, 3, 10],
     "min_samples_leaf" : [1, 3, 10],
     "bootstrap"        : [True, False],
-    "criterion"        : ["gini", "entropy"],
-    "n_estimators"     : [5, 10]
+    "criterion"        : ["gini", "entropy"]
 }
 
-clf = RandomForestClassifier()
+rf = RandomForestClassifier()
+clf = GridSearchCV(rf, param_grid=param_grid, n_jobs=-1, scoring='roc_auc')
