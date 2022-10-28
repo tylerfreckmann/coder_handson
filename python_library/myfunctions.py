90425f04f43468ab61388ace88ab35d9de0d1f17
tylerfreckmann <tyler.freckmann@gmail.com>
2022-10-28 09:51:54 -0700

    Add mock_drift

--- python_library/myfunctions.py
+++ python_library/myfunctions.py
@@ -1,3 +1,5 @@
+from random import random
+
 # Define function to bin column values.
 def bin_values(revenue, v):
     if revenue >= v:
@@ -7,3 +9,9 @@ def bin_values(revenue, v):
     else:
         val = revenue
     return val
+
+def mock_drift(v, drift_threshold):
+    if random() < drift_threshold:
+        return v * 1.25
+    else:
+        return v
